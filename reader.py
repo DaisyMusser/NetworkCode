b82ba326a8edfb87b3801436c021bafb6063e6e2
Quattro Musser <quattromusser@gmail.com>
2025-01-24 14:01:36 -0800

    sketched out how to build graph

--- reader.py
+++ reader.py
@@ -1,16 +1,25 @@
 import pandas as pd
+import networkx as nx
 
-# read into df
+# make an empty graph
+G = nx.Graph()
+
+# read csv into df
 df = pd.read_csv('./struct_data/csv/arrests.csv')
-
-# A 2d list of ids for folks with dup entries
-duplicates = []
 
 # add a name column without middle names
 df['simple_name'] = df['person_name'].str.replace(r' .* ', ' ', regex=True)
 
-# found one duplicate! Antoine [thomas] martin
-'''
+# duplicates only rows where folks are repeated
+# will be used to connect in graph later on ....
 duplicates = df[df['simple_name'].duplicated(keep=False)]
-print(duplicates)
-'''
+
+# build graph
+# for each arrest
+# make a graph of everyone in that arrest, connected
+# then combine graphs by merging nodes that represent the same person (probably)
+
+# use this format to add nodes with attributes...
+# G.add_nodes_from([(4, {"color": "red"}), (5, {"color": "green"})])
+
+
