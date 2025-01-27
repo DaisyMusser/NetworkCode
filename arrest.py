5f668a540e32d0f160e82b3e7034b775f776dba0
Quattro Musser <quattromusser@gmail.com>
2025-01-26 23:16:44 -0800

    wrote person and arrest class. Will be nodes and edges

--- arrest.py
+++ arrest.py
@@ -6,3 +6,9 @@ Represents an arrest. Has:
     + Street
 '''
 
+class Arrest:
+    def __init__(self, location_id, city, street):
+        self.location_id = location_id
+        self.city = city
+        self.street = street
+
--- person.py
+++ person.py
@@ -1,8 +1,15 @@
 
 '''
 Represents a person, has:
-    + person id[s]
-    + simple name
     + full name[s]
+    + simple name
 '''
+
+# Maybe helpful to convert from full name to simple
+# here? maybe not. think about it cutie ;)
+class Person:
+    def __init__(self, full_name, simple_name):
+        self.full_name   = full_name
+        self.simple_name = simple_name
+
 
