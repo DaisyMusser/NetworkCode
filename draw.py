27f91f65ea5bd1f71203ecd50e9853c550f8885f
Daisy <58235619+DaisyMusser@users.noreply.github.com>
2025-02-04 18:26:39 -0800

    found a cute seed for spring layout

--- draw.py
+++ draw.py
@@ -1,11 +1,14 @@
 import read 
 import matplotlib.pyplot as plt 
 import networkx as nx
+from networkx import drawing 
 
 
 def draw(filePath):
     G = read.makeGraph(filePath)
-    nx.draw(G)
+    nx.draw(G, pos=nx.spring_layout(G, seed=201))
     plt.show()
 
 draw('./struct_data/csv/test.csv')
+
+
--- testNetwork.py
+++ testNetwork.py
@@ -3,10 +3,11 @@ import read
 
 class testNetwork(unittest.TestCase):
 
-    def test_reader(self):
+    def test_read(self):
         G = read.makeGraph('./struct_data/csv/test.csv')
         self.assertEqual(12, len(G.nodes))
         self.assertEqual(10, len(G.edges))
+
 
 if __name__ == '__main__':
     unittest.main()
